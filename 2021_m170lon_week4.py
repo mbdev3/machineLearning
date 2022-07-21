671602f1c685683cfb44e5fc4e71d61a95fdf652
Mohammed Banani | MbDev <leedbn1@gmail.com>
2022-07-21 16:45:39 +0100

    ok

---
+++ 2021_m170lon_week4.py
@@ -0,0 +1,301 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: ad3940
+
+Based on the paper "Credit Card Fraud Detection - Machine Learning methods",
+Varmedja et al, 2019: https://ieeexplore.ieee.org/document/8717766
+"""
+
+
+#%% Classification model
+# https://machinelearningmastery.com/types-of-classification-in-machine-learning/
+
+#%% IMPORTING PACKAGES
+
+import pandas as pd # data processing
+import numpy as np # working with arrays
+import matplotlib.pyplot as plt # visualization
+from termcolor import colored as cl # text customization
+import seaborn as sns
+
+#from imblearn.over_sampling import RandomOverSampler,SMOTE, ADASYN
+from imblearn.over_sampling import SMOTE
+
+import warnings
+warnings.filterwarnings("ignore")
+
+from sklearn.preprocessing import StandardScaler # data normalization
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split # data split
+from sklearn.tree import DecisionTreeClassifier # Decision tree algorithm
+from sklearn.neighbors import KNeighborsClassifier # KNN algorithm
+from sklearn.linear_model import LogisticRegression # Logistic regression algorithm
+from sklearn.ensemble import RandomForestClassifier # Random forest tree algorithm
+from xgboost import XGBClassifier # XGBoost algorithm
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+
+from sklearn import metrics
+
+
+#%% IMPORTING DATA
+
+# df = pd.read_csv(r'C:\Users\Taoussi\Desktop\cw\AI\creditcard.csv')
+df = pd.read_csv(r'archive/creditcard.csv')
+print(df.head())
+
+
+
+df.shape
+
+#%% EDA
+
+# 1. Count & percentage
+
+cases = len(df)
+nonfraud_count = len(df[df.Class == 0])
+fraud_count = len(df[df.Class == 1])
+fraud_percentage = round(fraud_count/nonfraud_count*100, 2)
+
+print(cl('CASE COUNT : ', attrs = ['bold']))
+print(cl('--------------------------------------------', attrs = ['bold']))
+print(cl('Total number of cases are : {}'.format(cases), attrs = ['bold']))
+print(cl('Non-fraud cases are : {}'.format(nonfraud_count), attrs = ['bold'],))
+print(cl('Fraud cases are : {}'.format(fraud_count), attrs = ['bold']))
+print(cl('Percentage of fraud cases is : {} %'.format(fraud_percentage), attrs = ['bold'],color='green'))
+print(cl('--------------------------------------------', attrs = ['bold']))
+
+# 2. Description
+
+nonfraud_cases = df[df.Class == 0]
+fraud_cases = df[df.Class == 1]
+
+print(cl('CASE AMOUNT STATISTICS', attrs = ['bold']))
+print(cl('--------------------------------------------', attrs = ['bold']))
+print(cl('NON-FRAUD CASE AMOUNT STATS', attrs = ['bold']))
+print(nonfraud_cases.Amount.describe())
+print(cl('--------------------------------------------', attrs = ['bold']))
+print(cl('FRAUD CASE AMOUNT STATS', attrs = ['bold']))
+print(fraud_cases.Amount.describe())
+print(cl('--------------------------------------------', attrs = ['bold']))
+
+# 3. Distributions
+
+# Transaction Time Distribution
+
+plt.figure(figsize=(8,8))
+plt.title('Transaction Time Distributions')
+sns.distplot(df['Time'])
+plt.show()
+
+# Fraud Time Distribution
+
+fig, axs = plt.subplots(ncols=2, figsize=(16,4))
+
+sns.distplot(df[(df['Class'] == 1)]['Time'],bins=100,color='red', ax=axs[0])
+axs[0].set_title("Distribution of Fraud Transactions")
+
+sns.distplot(df[(df['Class'] == 0)]['Time'], bins=100,color='green', ax=axs[1])
+axs[1].set_title("Distribution of Non-Fraud Transactions")
+
+plt.show()
+
+# Scale Features (in this case "Amount")
+# https://sebastianraschka.com/Articles/2014_about_feature_scaling.html
+
+# Scale amount by log
+df['amount_log'] = np.log(df.Amount + 0.01)
+
+#Scale amount by Standardization
+ss = StandardScaler()
+df['amount_scaled'] = ss.fit_transform(df['Amount'].values.reshape(-1,1))
+# print("/////**********************",df['amount_scaled'])
+#Scale amount by Normalization
+norm = MinMaxScaler()
+df['amount_minmax'] = norm.fit_transform(df['Amount'].values.reshape(-1,1))
+
+#%% DATA SPLIT - SMOTE Dataset
+
+X = df.drop(['Class','Amount','amount_minmax','amount_log'],axis=1)
+y = df['Class']
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, shuffle=True)
+
+# print('*****',train_test_split(X, y, test_size = 0.2, shuffle=True))
+
+print("X_train: ",X_train.shape)
+print("y_train: ",y_train.shape)
+print("X_test: ",X_test.shape)
+print("y_test: ",y_test.shape)
+
+print('\n')
+print('............')
+print('\n')
+
+smote= SMOTE(sampling_strategy='minority')
+X_train_smote,y_train_smote=smote.fit_resample(X_train, y_train)
+X_test_smote, y_test_smote = X_test, y_test
+
+print("X_train_smote: ",X_train_smote.shape)
+print("y_train_smote: ",y_train_smote.shape)
+print("X_test_smote: ",X_test_smote.shape)
+print("y_test_smote: ",y_test_smote.shape)
+
+#%% METRICS
+"""
+For calculating the performance of each Classification model(with all the five 
+datasets), it would be really beneficial to create a function which could 
+evaluate all the metrics mentioned above and store them so that they could be
+compared later.
+"""
+
+names=[]
+aucs_tests = []
+accuracy_tests = []
+precision_tests = []
+recall_tests = []
+f1_score_tests = []
+mcc_score_tests = []
+
+def performance(model):
+    for name, model, X_train, y_train, X_test, y_test in model:
+        
+        #appending name
+        names.append(name)
+        
+        # Build model
+        model.fit(X_train, y_train)
+        
+        #predictions
+        y_test_pred = model.predict(X_test)
+        
+        # calculate accuracy
+        Accuracy_test = metrics.accuracy_score(y_test, y_test_pred)
+        accuracy_tests.append(Accuracy_test)
+        
+        # calculate auc
+        Aucs_test = metrics.roc_auc_score(y_test , y_test_pred)
+        aucs_tests.append(Aucs_test)
+        
+        #precision_calculation
+        Precision_score_test = metrics.precision_score(y_test , y_test_pred)
+        precision_tests.append(Precision_score_test)
+        
+        # calculate recall
+        Recall_score_test = metrics.recall_score(y_test , y_test_pred)
+        recall_tests.append(Recall_score_test)
+        
+        #calculating F1
+        F1Score_test = metrics.f1_score(y_test , y_test_pred)
+        f1_score_tests.append(F1Score_test)
+        
+        #calculating MCC
+        MCC_score_tests = metrics.matthews_corrcoef(y_test, y_test_pred)
+        mcc_score_tests.append(MCC_score_tests)
+        
+        # draw confusion matrix
+        cnf_matrix = metrics.confusion_matrix(y_test, y_test_pred) 
+        
+        print("Model Name :", name)
+        print('Test Accuracy :{0:0.5f}'.format(Accuracy_test))
+        print('Test AUC : {0:0.5f}'.format(Aucs_test))
+        print('Test Precision : {0:0.5f}'.format(Precision_score_test))
+        print('Test Recall : {0:0.5f}'.format(Recall_score_test))
+        print('Test F1 : {0:0.5f}'.format(F1Score_test))
+        print("The Matthews correlation coefficient is{0:0.5f}".format(MCC_score_tests))
+        print('Confusion Matrix : \n', cnf_matrix)
+        print("\n")
+
+        fpr, tpr, thresholds = metrics.roc_curve(y_test, y_test_pred)
+        auc = metrics.roc_auc_score(y_test, y_test_pred)
+        plt.plot(fpr,tpr,linewidth=2, label=name + ", auc="+str(auc))
+    
+    plt.legend(loc=4)
+    plt.plot([0,1], [0,1], 'k--' )
+    plt.rcParams['font.size'] = 12
+    plt.title('ROC curve')
+    plt.xlabel('False Positive Rate (1 - Specificity)')
+    plt.ylabel('True Positive Rate (Sensitivity)')
+    plt.show()
+
+#%% MODELING - CLASSIFICATION ALGORITHMS
+    
+# 1. Logical Regression Classifier
+
+
+LRmodel=[]
+
+LRmodel.append(('LR IMBALANCED', LogisticRegression(solver='saga',multi_class='multinomial'),X_train, y_train, X_test, y_test))
+LRmodel.append(('LR SMOTE', LogisticRegression(solver='saga',multi_class='multinomial'),X_train_smote, y_train_smote, X_test_smote, y_test_smote))
+
+performance(LRmodel)
+
+# 2. Random Forest Classifier
+
+RFmodel = []
+
+RFmodel.append(('RF IMABALANCED', RandomForestClassifier(),X_train,y_train,X_test,y_test))
+RFmodel.append(('RF SMOTE', RandomForestClassifier(),X_train_smote, y_train_smote, X_test_smote, y_test_smote))
+
+performance(RFmodel)
+
+
+# 3. Gaussian Naïve Bayes Classifier
+
+NBmodel = []
+
+NBmodel.append(('NB IMBALANCED', GaussianNB(),X_train,y_train,X_test,y_test))
+NBmodel.append(('NB SMOTE', GaussianNB(),X_train_smote, y_train_smote, X_test_smote, y_test_smote))
+
+performance(NBmodel)
+
+# 4. Decision Tree Classifier
+
+DTmodel = []
+
+DTmodel.append(('DT IMBALANCED', DecisionTreeClassifier(),X_train,y_train,X_test,y_test))
+DTmodel.append(('DT SMOTE', DecisionTreeClassifier(),X_train_smote, y_train_smote, X_test_smote, y_test_smote))
+
+performance(DTmodel)
+
+# 5. K-Nearest Neighbor Class
+
+KNNmodel = []
+
+KNNmodel.append(('KNN IMBALANCE', KNeighborsClassifier(),X_train,y_train,X_test,y_test))
+KNNmodel.append(('KNN SMOTE', KNeighborsClassifier(),X_train_smote, y_train_smote, X_test_smote, y_test_smote))
+
+performance(KNNmodel)
+
+# 6. XG Boost Classifier
+
+xgBOOST=[]
+xgBOOST.append(('XGBOOST IMBALANCED', XGBClassifier(n_estimators = 1000, verbosity = 1, scale_pos_weight = 580),X_train, y_train, X_test, y_test))
+xgBOOST.append(('XGBOOST SMOTE', XGBClassifier(n_estimators = 1000, verbosity = 1, scale_pos_weight = 580),X_train_smote, y_train_smote, X_test_smote, y_test_smote))
+
+performance(xgBOOST)
+
+# 7. MLP Classifier
+
+MLPclassifier=[]
+
+MLPclassifier.append(('MLPClassifier IMBALANCE', MLPClassifier(hidden_layer_sizes=(200,), max_iter=10000),X_train,y_train,X_test,y_test))
+MLPclassifier.append(('MLPClassifier SMOTE',MLPClassifier(hidden_layer_sizes=(200,), max_iter=10000),X_train_smote, y_train_smote, X_test_smote, y_test_smote))
+
+performance(MLPclassifier)
+
+# COMPARE MCC SCORE FOR ALL DATASETS
+comparison={
+    'Model': names,
+    'Accuracy': accuracy_tests,
+    'AUC': aucs_tests,
+    'Precision Score' : precision_tests,
+    'Recall Score': recall_tests, 
+    'F1 Score': f1_score_tests,
+    'MCC Score': mcc_score_tests
+}
+print("Comparing performance of various Classifiers: \n \n")
+comparison=pd.DataFrame(comparison)
+comparison.sort_values('MCC Score',ascending=False)
---
+++ README.md
@@ -0,0 +1,10 @@
+### Data:
+
+https://www.kaggle.com/datasets/mlg-ulb/creditcardfraud
+
+---
+
+```
+python -m notebook
+
+```
